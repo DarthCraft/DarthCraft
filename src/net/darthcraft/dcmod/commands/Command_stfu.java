a8da4254891ce6f1b919b0f4f312da13212fb9e3
Ryan <Admin@superior-networks.com>
2014-07-25 22:31:06 +0100

    As requested, added a STFU command.

---
+++ src/net/darthcraft/dcmod/commands/Command_stfu.java
@@ -0,0 +1,43 @@
+package net.darthcraft.dcmod.commands;
+
+import net.pravian.bukkitlib.command.SourceType;
+import net.pravian.bukkitlib.util.PlayerUtils;
+import net.darthcraft.dcmod.addons.PlayerManager.PlayerInfo;
+import net.darthcraft.dcmod.commands.Permissions.Permission;
+import net.darthcraft.dcmod.commands.Permissions.PermissionUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@Source(SourceType.ANY)
+@Permissions(Permission.LEGACY)
+public class Command_stfu extends DarthCraftCommand {
+
+    @Override
+    public boolean run(CommandSender sender, Command cmd, String[] args) {
+        if (args.length != 1) {
+            return showUsage(cmd);
+        }
+
+        Player player = PlayerUtils.getPlayer(args[0]);
+        if (player == null) {
+            return warn("Player not found!");
+        }
+
+        if (PermissionUtils.hasPermission(player, Permission.ADMIN)) {
+            if (!PermissionUtils.hasPermission(sender, Permission.HEADADMIN)) {
+                return warn("You may not mute that player.");
+            }
+        }
+
+        PlayerInfo info = plugin.playerManager.getInfo(player);
+
+        if (info.isMuted()) {
+            return warn("That player is already muted.");
+        }
+
+        util.adminAction(sender, "Muting " + player.getName());
+        info.setMuted(true);
+        return true;
+    }
+}
