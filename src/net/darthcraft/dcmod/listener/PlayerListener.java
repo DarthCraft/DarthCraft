d93be88fbad848e2da0fc4136027c06e5bf2fbf8
Ryan <Admin@superior-networks.com>
2014-07-25 22:56:48 +0100

    Corrected the head admin and host colours, added possible admin and premium colours.

--- src/net/darthcraft/dcmod/listener/PlayerListener.java
+++ src/net/darthcraft/dcmod/listener/PlayerListener.java
@@ -12,60 +12,75 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import net.darthcraft.dcmod.commands.Permissions.Permission;
+import net.darthcraft.dcmod.commands.Permissions.PermissionUtils;
 
-public class PlayerListener implements Listener {
+public class PlayerListener implements Listener
+    {
 
     private final DarthCraft plugin;
 
-    public PlayerListener(DarthCraft plugin) {
+    public PlayerListener(DarthCraft plugin)
+        {
         this.plugin = plugin;
-    }
-    
-    
+        }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
-    public void onPlayerChat(final AsyncPlayerChatEvent event) {
+    public void onPlayerChat(final AsyncPlayerChatEvent event)
+        {
         plugin.trollMode.onPlayerChat(event);
         plugin.adminChat.onPlayerChat(event);
         plugin.adminBusy.onPlayerChat(event);
         plugin.chatFilter.onPlayerChat(event);
-    }
+        }
 
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerQuit(PlayerQuitEvent event) {
+    public void onPlayerQuit(PlayerQuitEvent event)
+        {
         plugin.playerManager.onPlayerQuit(event);
-    }
+        }
 
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerLogin(PlayerLoginEvent event) {
+    public void onPlayerLogin(PlayerLoginEvent event)
+        {
         plugin.forceIp.onPlayerLogin(event);
         plugin.banManager.onPlayerLogin(event);
-    }
+        }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onUncancelledPlayerJoin(PlayerJoinEvent event) {
+    public void onUncancelledPlayerJoin(PlayerJoinEvent event)
+        {
         plugin.playerManager.onUncancelledPlayerJoin(event);
         plugin.banWarner.onUncancelledPlayerJoin(event);
-    }
+        }
 
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onPlayerInteract(PlayerInteractEvent event) {
+    public void onPlayerInteract(PlayerInteractEvent event)
+        {
         plugin.likeSigns.onPlayerInteractEvent(event);
-    }
-    
+        }
+
     // Player Tab colours.
     @EventHandler(priority = EventPriority.HIGH)
     public static void onPlayerJoinEvent(PlayerJoinEvent event)
-    {
+        {
         Player player = event.getPlayer();
-        if (Util.HOSTS.contains(player.getName()))
-        {
+        if (Util.HOSTS.contains(player.getName()) || Util.HEADADMINS.contains(player.getName()))
+            {
             player.setPlayerListName(ChatColor.DARK_PURPLE + player.getName());
-        }
-        if (Util.HEADADMINS.contains(player.getName()))
-        {
+            }
+
+        // This stuff MIGHT work, I however have never tried and have yet to test it, so it may not work :/
+        else if (PermissionUtils.hasPermission(player, Permission.ADMIN))
+            {
+            player.setPlayerListName(ChatColor.RED + player.getName());
+            }
+
+        else if (PermissionUtils.hasPermission(player, Permission.PREMIUM))
+            {
             player.setPlayerListName(ChatColor.LIGHT_PURPLE + player.getName());
+            }
+
         }
+
     }
-    
-}
