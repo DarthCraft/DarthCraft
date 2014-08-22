d720f0fc63ac02ef92e4a83f1ba20b038dc221fa
Ryan <Admin@superior-networks.com>
2014-08-22 02:13:09 +0100

    Made some changes to how /busy works including including commands and broadcasting when a admin is busy.

--- src/net/darthcraft/dcmod/player/AdminBusy.java
+++ src/net/darthcraft/dcmod/player/AdminBusy.java
@@ -1,14 +1,13 @@
 package net.darthcraft.dcmod.player;
 
-import net.darthcraft.dcmod.player.PlayerManager;
 import net.darthcraft.dcmod.DarthCraft;
-import net.darthcraft.dcmod.addons.DarthCraftAddon;
 import net.darthcraft.dcmod.addons.DarthCraftAddon;
 import net.darthcraft.dcmod.commands.Permissions;
 import net.darthcraft.dcmod.commands.Permissions.PermissionUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class AdminBusy extends DarthCraftAddon
     {
@@ -26,10 +25,39 @@ public class AdminBusy extends DarthCraftAddon
 
         info.setBusy(!info.isBusy());
 
-        util.msg(player, ChatColor.AQUA + "Toggled busy status o" + (info.isBusy() ? "n" : "ff"));
+        util.msg(player, ChatColor.AQUA + "You have toggled busy status o" + (info.isBusy() ? "n" : "ff"));
+        util.adminAction(player, ChatColor.AQUA + "Toggled busy status o" + (info.isBusy() ? "n" : "ff"));
         }
 
     public void onPlayerChat(AsyncPlayerChatEvent event)
+        {
+        final String[] words = event.getMessage().split(" ");
+        for (final String word : words)
+            {
+            if (word.length() < MIN_WORD_LENGTH)
+                {
+                continue;
+                }
+
+            final Player player = server.getPlayer(word);
+            if (player == null)
+                {
+                continue;
+                }
+
+            if (!PermissionUtils.hasPermission(player, Permissions.Permission.ADMIN))
+                {
+                return;
+                }
+
+            if (plugin.playerManager.getInfo(player).isBusy())
+                {
+                plugin.util.sendSyncMessage(event.getPlayer(), ChatColor.RED + player.getName() + " is busy right now, try again later");
+                }
+            }
+        }
+
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event)
         {
         final String[] words = event.getMessage().split(" ");
         for (final String word : words)
