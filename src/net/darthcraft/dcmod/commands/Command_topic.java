e00a891f835f7e7d60d334005ef52a3d402bc0b8
Ryan <Admin@superior-networks.com>
2015-01-02 18:55:51 +0000

    Done some styling to the topics though cant manage to make individual topics individual colours. Resolves #27

--- src/net/darthcraft/dcmod/commands/Command_topic.java
+++ src/net/darthcraft/dcmod/commands/Command_topic.java
@@ -1,6 +1,8 @@
 package net.darthcraft.dcmod.commands;
 
+import net.darthcraft.dcmod.DC_Utils;
 import net.pravian.bukkitlib.command.SourceType;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -13,7 +15,7 @@ public class Command_topic extends DarthCraftCommand
     public boolean run(CommandSender sender, Command cmd, String[] args)
         {
 
-        sender.sendMessage(plugin.topicsConfig.getString("topicmessage") + " " + plugin.topicGenerator.getTopic());
+        sender.sendMessage(DC_Utils.colorize("&8[&4DarthCraft Plugin&8] " + plugin.topicsConfig.getString("topicmessage") + " " + ChatColor.DARK_AQUA + plugin.topicGenerator.getTopic()));
         return true;
         }
 
