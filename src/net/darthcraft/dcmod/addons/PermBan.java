eb14840df8499a0c5020846ca24c568b8e085132
Ryan <Admin@superior-networks.com>
2015-01-18 16:12:18 +0000

    Updated the hard coded perm banned players.

--- src/net/darthcraft/dcmod/addons/PermBan.java
+++ src/net/darthcraft/dcmod/addons/PermBan.java
@@ -18,9 +18,14 @@ public class PermBan extends DarthCraftAddon
         Player player = event.getPlayer();
 
         // Hard Coded Perm-Ban
-        if (player.getUniqueId().toString().equals("76093c08-4054-4a54-95c5-961bcfa768c6"))
+        switch (player.getUniqueId().toString())
         {
-            event.disallow(PlayerLoginEvent.Result.KICK_OTHER, ChatColor.RED + "FlamedBulletLuke, May I congratulate you on being the first person to be hard-coded into the DarthCraftMod's permban system. I hope you think carefully before threatening this server.");
+            case "76093c08-4054-4a54-95c5-961bcfa768c6":
+                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, ChatColor.RED + "FlamedBulletLuke, May I congratulate you on being the first person to be hard-coded into the DarthCraftMod's permban system. I hope you think carefully before threatening this server.");
+                break;
+            case "6ae9cb8f-5db0-4dc7-bac2-454d33b967af":
+                event.disallow(PlayerLoginEvent.Result.KICK_OTHER, ChatColor.RED + "Well NoobMan, it appears that you just killed a hosts animals. May I just inform you of the stupity of that idea, as you can never join this server again. Bravo.  ");
+                break;
         }
 
     }
