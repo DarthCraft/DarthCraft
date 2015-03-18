0b292d99fb0901c1bb950163c5b794a7577f8d83
Ryan <Ryan@RYAN-LAPTOP>
2015-03-18 14:39:38 +0000

    Added an admin vanish command (/hidemyass) Resolves #53

---
+++ src/net/darthcraft/dcmod/commands/Command_hidemyass.java
@@ -0,0 +1,37 @@
+package net.darthcraft.dcmod.commands;
+
+import net.pravian.bukkitlib.command.SourceType;
+import net.pravian.bukkitlib.util.PlayerUtils;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@Source(value = SourceType.PLAYER)
+@Permissions(value = Permissions.Permission.ADMIN)
+public class Command_hidemyass extends DarthCraftCommand
+{
+
+    @Override
+    public boolean run(CommandSender sender, Command cmd, String[] args)
+    {
+        if (!(sender instanceof Player))
+        {
+            sender.sendMessage("You must be a player to execute that command!");
+            return true;
+        }
+        Player player = (Player) sender;
+
+        if (player.getGameMode() != GameMode.SPECTATOR)
+        {
+            player.setGameMode(GameMode.SPECTATOR);
+        }
+        else
+        {
+            player.setGameMode(GameMode.SURVIVAL);
+        }
+
+        return true;
+
+    }
+}
