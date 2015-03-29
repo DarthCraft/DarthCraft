e70576c9411c4901afd81c1348836338b94bec02
Ryan <Admin@superior-networks.com>
2015-03-30 00:57:29 +0100

    Updated the darthcraft message. Resolves 58

--- src/net/darthcraft/dcmod/commands/Command_darthcraft.java
+++ src/net/darthcraft/dcmod/commands/Command_darthcraft.java
@@ -1,7 +1,6 @@
 package net.darthcraft.dcmod.commands;
 
 import net.darthcraft.dcmod.commands.Permissions.Permission;
-import net.darthcraft.dcmod.commands.Permissions.PermissionUtils;
 import net.pravian.bukkitlib.command.SourceType;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -18,77 +17,29 @@ public class Command_darthcraft extends DarthCraftCommand
         int unique = plugin.mainConfig.getInt("uniqueplayers");
         int players = plugin.mainConfig.getInt("players");
 
-        if (args.length == 0)
-        {
-            sender.sendMessage(ChatColor.GRAY + "-- " + ChatColor.DARK_PURPLE + "Darth" + ChatColor.LIGHT_PURPLE + "Craft" + ChatColor.GRAY + " --");
-            sender.sendMessage(ChatColor.DARK_PURPLE + "Darth" + ChatColor.LIGHT_PURPLE + "Craft" + ChatColor.GOLD + " is a community-focussed,"
-                    + " non-PvP survival server.");
-            sender.sendMessage(ChatColor.GOLD + "It is made possible by a long list of names of whom which developers, administrators and "
-                    + "other staff who can't all be named.");
-            sender.sendMessage(ChatColor.RED + "Special thanks to:");
-            sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "KimJongBen" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Owner, Host, Developer" + ChatColor.WHITE + ")");
-            sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "Wild1145" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Host, Lead Developer" + ChatColor.WHITE + ")");
-            sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "KickAssScott" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Host, Game manager" + ChatColor.WHITE + ")");
-            sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "DarthSalamon" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Exiled, Developer" + ChatColor.WHITE + ")");
-            sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "JabbaTheJake" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Head-Admin" + ChatColor.WHITE + ")");
-            sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "boulos77" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Head-Admin" + ChatColor.WHITE + ")");
-            sender.sendMessage(ChatColor.GOLD + "There have been a total of " + ChatColor.DARK_RED + unique + ChatColor.GOLD + " unique players and a total of " + ChatColor.DARK_RED + players + " player joins to the DarthCraft Server.");
-            return true;
-        }
-
-        if (args.length == 1)
-        {
-            if (args[0].equalsIgnoreCase("plugin"))
-            {
-                sender.sendMessage(ChatColor.GOLD + plugin.pluginName + " for 'DarthCraft', a community-focussed non-PvP server.");
-                sender.sendMessage(ChatColor.GOLD + String.format("Version " + ChatColor.BLUE + "%s.%s" + ChatColor.BLUE + ", built %s.",
-                        plugin.pluginVersion,
-                        plugin.pluginBuildNumber,
-                        plugin.pluginBuildDate));
-                sender.sendMessage(ChatColor.GOLD + "Created by: " + plugin.pluginAuthors);
-                sender.sendMessage(ChatColor.GREEN + "Visit " + ChatColor.AQUA + "http://darthcraft.net/" + ChatColor.GREEN + " for more information.");
-                return true;
-            }
-
-            if (args[0].equalsIgnoreCase("reload"))
-            {
-                if (!PermissionUtils.hasPermission(sender, Permission.HOST))
-                {
-                    return noPerms();
-                }
-
-                util.adminAction(sender, "Reloading DarthCraft config");
-
-                plugin.mainConfig.load();
-
-                // Debug-mode
-                plugin.logger.setDebugMode(plugin.mainConfig.getBoolean("debug"));
-                plugin.logger.debug("Debug-mode enabled!");
-
-                // Disable the plugin if the config defines so
-                if (!plugin.mainConfig.getBoolean("enabled", true))
-                {
-                    plugin.logger.warning("Disabling: defined in config");
-                    plugin.getServer().getPluginManager().disablePlugin(plugin);
-                    return true;
-                }
-
-                // Load configs
-                plugin.bansConfig.load();
-                plugin.likersConfig.load();
-
-                // Cache items from config files
-                plugin.banManager.loadBans();
-                plugin.trollMode.loadSettings();
-                plugin.chatFilter.loadSettings();
-                plugin.forceIp.loadSettings();
-                plugin.likeSigns.loadSettings();
-
-                sender.sendMessage("Finished reloading " + plugin.pluginName + " v" + plugin.pluginVersion + "." + plugin.pluginBuildNumber);
-                return true;
-            }
-
-        }
+        sender.sendMessage(ChatColor.DARK_AQUA + "--" + ChatColor.GOLD + "Darth" + ChatColor.LIGHT_PURPLE + "Craft" + ChatColor.DARK_AQUA + " Server" + ChatColor.DARK_AQUA + "--");
+        sender.sendMessage("");
+        sender.sendMessage(ChatColor.GOLD + "The " + ChatColor.GOLD + "Darth" + ChatColor.LIGHT_PURPLE + "Craft" + ChatColor.GOLD + " server is a community-focused, Non-PVP Server");
+        sender.sendMessage(ChatColor.GOLD + "The server has been designed with the players in mind, and we have a lot of people who have made the server what it is today, and we thank them all.");
+        sender.sendMessage(ChatColor.GOLD + "We would like to thank all the players and admins for making the server such a friendly and amazing place to play, and we thank you for your continued effort with the server.");
+        sender.sendMessage(ChatColor.GOLD + "We would also like to give special thanks to the following players, who have made an outstanding contribution to the DarthCraft Server:");
+        sender.sendMessage("");
+        sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "KimJongBen" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Owner, Host, Developer" + ChatColor.WHITE + ")");
+        sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "Wild1145" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Host, Lead Developer" + ChatColor.WHITE + ")");
+        sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "KickAssScott" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Host, Game manager" + ChatColor.WHITE + ")");
+        sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "DarthSalamon" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Founder" + ChatColor.WHITE + ")");
+        sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "JabbaTheJake" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Head-Admin" + ChatColor.WHITE + ")");
+        sender.sendMessage(ChatColor.DARK_GRAY + "- " + ChatColor.DARK_GREEN + "boulos77" + ChatColor.WHITE + " (" + ChatColor.YELLOW + "Head-Admin" + ChatColor.WHITE + ")");
+        sender.sendMessage("");
+        sender.sendMessage(ChatColor.DARK_BLUE + "The server you are playing has had " + ChatColor.GOLD + unique + ChatColor.DARK_BLUE + " unique players and " + ChatColor.GOLD + players + ChatColor.DARK_BLUE + " different player joins since the plugin was added.");
+        sender.sendMessage("");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "The DarthCraft plugin was built for the DarthCraft server, here are some statistics about the plugin currently running on your server:");
+        sender.sendMessage(ChatColor.DARK_RED + "Build Version: " + plugin.pluginVersion);
+        sender.sendMessage(ChatColor.DARK_RED + "Build Date: " + plugin.pluginBuildDate);
+        sender.sendMessage(ChatColor.DARK_RED + "Build Number: " + plugin.pluginBuildNumber);
+        sender.sendMessage(ChatColor.DARK_RED + "Plugin Authors: " + plugin.pluginAuthors);
+        sender.sendMessage("");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "For more information, please see our website: " + ChatColor.AQUA + "http://www.darthcraft.net");
 
         return showUsage(cmd);
     }
