ffbdef004d36c60c59ade509e04149efcca40cb3
Ryan <Ryan@RYAN-DESKTOP>
2015-11-14 17:45:38 +0000

    Restarted using the new Pravian Aero Framework

--- src/net/darthcraft/darthcraftmod/DarthCraftMod.java
+++ src/net/darthcraft/darthcraftmod/DarthCraftMod.java
@@ -1,7 +1,28 @@
 package net.darthcraft.darthcraftmod;
 
-public class DarthCraftMod
-{
+import net.pravian.aero.plugin.AeroPlugin;
 
-   
-}
+public class DarthCraftMod extends AeroPlugin<DarthCraftMod>
+  {
+
+    public static DarthCraftMod plugin;
+
+    @Override
+    public void load()
+    {
+        DarthCraftMod.plugin = this;
+    }
+
+    @Override
+    public void enable()
+    {
+        DarthCraftMod.plugin = this;
+    }
+
+    @Override
+    public void disable()
+    {
+        DarthCraftMod.plugin = null;
+    }
+
+  }
